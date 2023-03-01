516c4216c5f06da553dae852e3df5aef41b898a1
Marion <sousagracamarion@gmail.com>
2023-03-01 16:50:29 +0100

    Ajout du portfolio Marion

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="portfolio.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  <title>Marion Sousa Graça Frontend Web Developer</title>
+  <meta name="description"
+    content="Une developpeuse web junior" />
+  <meta name="author" content="Marion Sousa Graça" />
+  <meta name="google-site-verification" content="wX2xdkxqjrC4rA2EM-9bbaVpiEEFG8ae5azgeZDd-oo" />
+
+  <meta property="og:title" content="Marion Sousa Graça | RammCodes | Frontend Web Developer" />
+  <meta property="og:site_name" content="Marion Sousa Graça Portfolio Website" />
+  <meta property="og:url" content="https://www.marionsousagraca.com/" />
+  <meta property="og:description"
+    content="Junior Frontend focused Web Developer" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="https://i.ibb.co/C8Wnkrt/pretty-snap-2022-3-29-16-11.png" />
+
+  <link href='https://d33wubrfki0l68.cloudfront.net/css/134159683fc0e467a64ae99e3aa3783a3daaf7e2/css/style.css' rel='stylesheet'/>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QD9ESD1D9K"></script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'G-QD9ESD1D9K')
+  </script>
+</head>
+<body>
+  <header class="header">
+  <div class="header__content">
+  <div class="header__logo-container">
+  <div class="header__logo-img-cont">
+  <img id="photo-face" src="images/photo-face-2.jpg" />
+  </div>
+  <a href="./"><span class="header__logo-sub" onmouseover="this.style.color='green'"
+onmouseout="this.style.color='black'">Marion Sousa Graça</span></a>
+  </div>
+  <div class="header__main">
+  <ul class="header__links">
+  <li class="header__link-wrapper">
+  <a href="./" class="header__link" onmouseover="this.style.color='green'"
+onmouseout="this.style.color='black'"> Accueil </a>
+  </li>
+  <li class="header__link-wrapper">
+  <a href="./#about" class="header__link" onmouseover="this.style.color='green'"
+onmouseout="this.style.color='black'">A propos </a>
+  </li>
+  <li class="header__link-wrapper">
+  <a href="./#projects" class="header__link" onmouseover="this.style.color='green'"
+onmouseout="this.style.color='black'"> Projets </a>
+  </li>
+  <li class="header__link-wrapper">
+  <a href="./#contact" class="header__link" onmouseover="this.style.color='green'"
+onmouseout="this.style.color='black'"> Contact </a>
+  </li>
+  </ul>
+  <div class="header__main-ham-menu-cont">
+  <img src="https://d33wubrfki0l68.cloudfront.net/79e75114856ae61628d2ad26504e3ff4ab2c71b6/f06a4/assets/svg/ham-menu.svg" alt="hamburger menu" class="header__main-ham-menu" />
+  <img src="https://d33wubrfki0l68.cloudfront.net/de2a681c8ca1630b29949b3a34bf158a686a0554/6a0ec/assets/svg/ham-menu-close.svg" alt="hamburger menu close"
+            class="header__main-ham-menu-close d-none" />
+  </div>
+  </div>
+  </div>
+  <div class="header__sm-menu">
+  <div class="header__sm-menu-content">
+  <ul class="header__sm-menu-links">
+  <li class="header__sm-menu-link">
+  <a href="./"> Home </a>
+  </li>
+
+  <li class="header__sm-menu-link">
+  <a href="./#about"> About </a>
+  </li>
+
+  <li class="header__sm-menu-link">
+  <a href="./#projects"> Projects </a>
+  </li>
+
+  <li class="header__sm-menu-link">
+  <a href="./#contact"> Contact </a>
+  </li>
+  </ul>
+  </div>
+  </div>
+  </header>
+  <section class="home-hero" id="fond-blanc">
+  <div class="home-hero__content">
+  <h1 class="heading-primary">Bonjour, je suis Marion Sousa Graça</h1>
+  <div class="home-hero__info">
+  <p class="text-primary">
+        Je suis apprentie développeuse web junior, ici pour tout apprendre à coder et faire de la programmation en général.
+  </p>
+  </div>
+  <div class="home-hero__cta">
+  <a href="./#projects" class="btn btn--bg" style="background-color:#125223; color:#fff">Projets</a>
+  </div>
+  </div>
+  <div class="home-hero__socials">
+  <div class="home-hero__social">
+  <a href="https://www.linkedin.com/in/marion-sousa-gra%C3%A7a-347a43246/" class="home-hero__social-icon-link" rel="noreferrer"
+          target="_blank">
+  <img src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg" alt="Marion Sousa Graça Linkedin Profile"
+            class="home-hero__social-icon" />
+  </a>
+  </div>
+  <div class="home-hero__social">
+  <a href="https://github.com/Marion404" class="home-hero__social-icon-link" rel="noreferrer" target="_blank">
+  <img src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg" alt="RammCodes Github Profile" class="home-hero__social-icon" />
+  </a>
+  </div>
+  </div>
+  </section>
+  <section id="about" class="about sec-pad">
+  <div class="main-container">
+  <h2 class="heading heading-sec heading-sec__mb-med">
+  <span class="heading-sec__main">A propos</span>
+  <span class="heading-sec__sub">
+         Ici vous trouverez toutes les informations concernant mes compétences professionnelles en programmation.
+  </span>
+  </h2>
+  <div class="about__content">
+  <div class="about__content-main">
+  <h3 class="about__content-title">Présentation</h3>
+  <div class="about__content-details">
+  <p class="about__content-details-para">
+             Apprentie développeuse web spécialisée en <strong> frontend</strong>. J'apprends à réaliser  différents types de sites web (sites simples, QCM, animations simples, formulaires, chats). A consulter dans la section<strong> Projets.</strong>
+  </p>
+  <p class="about__content-details-para">
+             Vous pouvez consulter mon profil LinkedIn ici:
+              <a rel="noreferrer" href="https://www.linkedin.com/in/marion-sousa-gra%C3%A7a-347a43246/" target="_blank" style="color:#125223;">Linkedin</a>
+              pour plus de détails sur mon parcours professionnel global, au delà du code et du développement de logiciel.
+  </p>
+  <p class="about__content-details-para">
+             Je suis prête à <strong> travailler </strong> pour une entreprise avec laquelle je pourrais continuer à apprendre, gagner en expérience et m'épanouir. Alors n'hésitez à me <strong> contacter </strong> pour vous joindre votre firme.
+  </p>
+  </div>
+  <a href="./#contact" class="btn btn--med btn--theme dynamicBgClr" style="background-color:#125223; color:#fff">Contact</a>
+  </div>
+  <div class="about__content-skills">
+  <h3 class="about__content-title">Mes compétences</h3>
+  <div class="skills">
+  <div class="skills__skill">HTML</div>
+  <div class="skills__skill">CSS</div>
+  <div class="skills__skill">JavaScript</div>
+  <div class="skills__skill">React</div>
+  <div class="skills__skill">SASS</div>
+  <div class="skills__skill">GIT</div>
+  <div class="skills__skill">Github</div>
+  <div class="skills__skill">Responsive Design</div>
+  <div class="skills__skill">SEO</div>
+  <div class="skills__skill">POO</div>
+  <div class="skills__skill">SQL Basics</div>
+  </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="projects" class="projects sec-pad">
+    <div class="main-container">
+      <h2 class="heading heading-sec heading-sec__mb-bg">
+        <span class="heading-sec__main">Projets</span>
+        <span class="heading-sec__sub">
+        Ici vous pouvez consulter mes différents projets réalisés en cours de formation:
+        </span>
+      </h2>
+      <div class="projects__content">
+        <div class="projects__row">
+          <div class="projects__row-img-cont">
+            <img src="images/one-million-lines-site.png" alt="Software Screenshot" class="projects__row-img"
+              loading="lazy" />
+          </div>
+          <div class="projects__row-content">
+            <h3 class="projects__row-content-title">One Million Lines</h3>
+            <p class="projects__row-content-desc">
+            One million lines fut notre 1ère maquette de site simple à peu d'éléments. Elle inclut: Un fond, des logos, des pictogrammes et des boutons.
+            </p>
+            <a class='btn btn--med btn--theme dynamicBgClr' href='https://marionsg-404-one-million-lines.projets.garage404.com/' target='_blank'style="background-color:#125223; color:#fff">Consulter</a>
+          </div>
+        </div>
+        <div class="projects__row">
+          <div class="projects__row-img-cont">
+            <img src="images/the-mountains.png" alt="Software Screenshot" class="projects__row-img"
+              loading="lazy" />
+          </div>
+          <div class="projects__row-content">
+            <h3 class="projects__row-content-title">The Mountain</h3>
+            <p class="projects__row-content-desc">
+              Site similaire mais un peu plus complexe que "One Million Lines". Avec plus d'images de tailles différentes à agencer de façon précise, et plus de boutons.
+            </p>
+            <a class='btn btn--med btn--theme dynamicBgClr' href='https://marion-the-mountain.projets.garage404.com/' target='_blank'style="background-color:#125223; color:#fff">Consulter</a>
+          </div>
+        </div>
+        <div class="projects__row">
+          <div class="projects__row-img-cont">
+            <img src="images/drum-pad-beatbox.png" alt="Software Screenshot" class="projects__row-img"
+              loading="lazy" />
+          </div>
+          <div class="projects__row-content">
+            <h3 class="projects__row-content-title">Drum Pad</h3>
+            <p class="projects__row-content-desc">
+              Exercice visuel et sonore. On apprend à réaliser une "batterie virtuelle" en carré de 9 percussions, chacunes avec son nom précis.
+            </p>
+            <a class='btn btn--med btn--theme dynamicBgClr' href='https://marionsg-404-drum-pad.projets.garage404.com/' target='_blank'style="background-color:#125223; color:#fff">Consulter</a>
+          </div>
+        </div>
+        <div class="projects__row">
+          <div class="projects__row-img-cont">
+            <img src="images/animation-mario.png" alt="Software Screenshot" class="projects__row-img"
+              loading="lazy" />
+          </div>
+          <div class="projects__row-content">
+            <h3 class="projects__row-content-title">Animations</h3>
+            <p class="projects__row-content-desc">
+              TP d'animation en CSS. Pour animer 2 personnages, un décor et d'autres détails avec des actions et vitesses différentes.
+            </p>
+            <a class='btn btn--med btn--theme dynamicBgClr' href='https://marionsg-404-animation-mario.projets.garage404.com/' target='_blank'style="background-color:#125223; color:#fff">Consulter</a>
+          </div>
+        </div>
+        <div class="projects__row">
+          <div class="projects__row-img-cont">
+            <img src="images/quiz-anime.png" alt="Software Screenshot" class="projects__row-img"
+              loading="lazy" />
+          </div>
+          <div class="projects__row-content">
+            <h3 class="projects__row-content-title">Quiz QCM</h3>
+            <p class="projects__row-content-desc">
+            Un quiz qcm en 2 pages, (Page de connection puis page du quiz) avec affichage d'images "OnClick" dans le quiz en PHP.
+            </p>
+            <a class='btn btn--med btn--theme dynamicBgClr' href='https://marion-404-quiz-anime.projets.garage404.com/' target='_blank'style="background-color:#125223; color:#fff">Consulter</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="home-hero" id="contact" style="background: url(images/fond-blanc.jpg);
+}">
+    <div class="main-container">
+      <h2 class="heading heading-sec heading-sec__mb-med">
+        <span class="heading-sec__main heading-sec__main--lt">Contact</span>
+        <span class="heading-sec__sub heading-sec__sub--lt">
+          Vous pouvez me contacter facilement en remplissant le formulaire ci-dessous:
+        </span>
+      </h2>
+      <div class="contact__form-container">
+        <form action='#' class='contact__form' method='post'><input type='hidden' name='form-name' value='form 1' />
+          <div class="contact__form-field">
+            <label class="contact__form-label" for="name">Nom</label>
+            <input required placeholder="Entrez votre nom" type="text" class="contact__form-input" name="name"
+              id="name" />
+          </div>
+          <div class="contact__form-field">
+            <label class="contact__form-label" for="email">Email</label>
+            <input required placeholder="Entrez votre Email" type="email" class="contact__form-input" name="email"
+              id="email" />
+          </div>
+          <div class="contact__form-field">
+            <label class="contact__form-label" for="message">Message</label>
+            <textarea required cols="30" rows="10" class="contact__form-input1" placeholder="Entrez votre Message"
+              name="message" id="message"></textarea>
+          </div>
+          <button type="submit" class="btn btn--theme contact__btn" style="background-color:#125223; color:#fff">
+            Envoyer
+          </button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <footer class="main-footer">
+    <div class="main-container">
+      <div class="main-footer__upper">
+        <div class="main-footer__row main-footer__row-1">
+          <h2 class="heading heading-sm main-footer__heading-sm">
+            <span>Social</span>
+          </h2>
+          <div class="main-footer__social-cont">
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/marion-sousa-gra%C3%A7a-347a43246/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg>
+            </a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/Marion404">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="main-footer__row main-footer__row-2">
+          <h2 class="heading heading-sm text-lt">Marion Sousa Graça</h2>
+          <p class="main-footer__short-desc">
+          Apprentie développeuse web spécialisée en <strong> frontend</strong>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src='https://d33wubrfki0l68.cloudfront.net/js/96f5c0d4b9bdbf07570efd8e94cb7db7a5191a01/index.js'></script>
+  
+</body>
+
+</html>
